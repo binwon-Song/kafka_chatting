544e6febcf196c3743d206e5fd2a81da925b59db
binwon-song <binwon236@gmail.com>
2023-08-07 17:30:43 +0900

    Add init kafka

---
+++ producer.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const app=express()
+const port=3000
+const {Kafka} = require('kafkajs')
+
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+
+
+const kafka=new Kafka({
+    clientId:'my-app',
+    brokers:['localhost:9092']
+})
+const producer=kafka.producer()
+const initKafka=async()=>{
+    await producer.connect()
+}
+
+app.post('/events/:event',async(req,res)=>{
+    await producer.send({
+        topic:'quickstart-events',
+        messages:[
+            {value:req.params.event},
+
+        ],
+    })
+    res.send('event : '+req.params.event+'\n')
+})
+
+app.listen(port,async ()=>{
+    console.log(`kafca app listening on port ${port}`)
+})
+initKafka();
